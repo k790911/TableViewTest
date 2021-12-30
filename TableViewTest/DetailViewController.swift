70e2ea6348177d122f719cf4c4db25077146da87
k790911 <k790911@gmail.com>
2021-12-30 22:01:46 +0900

    12월 30일 테이블뷰 테스트

---
+++ TableViewTest/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  TableViewTest
+//
+//  Created by 김재훈 on 2021/12/30.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var lblItem: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
